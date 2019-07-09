c783138030f072704b22ab7ae7712dee6eaa1079
SilentHackerz <ayyubrazazain@gmail.com>
2019-07-09 19:35:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AddFriend extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      newFriend: ''
+    }
+
+    this.updateNewFriend = this.updateNewFriend.bind(this)
+    this.handleAddNew = this.handleAddNew.bind(this)
+  }
+  updateNewFriend(e) {
+    this.setState({
+      newFriend: e.target.value
+    })
+  }
+  handleAddNew() {
+    this.props.addNew(this.state.newFriend)
+    this.setState({
+      newFriend: ''
+    })
+  }
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.newFriend}
+          onChange={this.updateNewFriend}
+        />
+        <button onClick={this.handleAddNew}> Add Friend </button>
+      </div>
+    )
+  }
 }
 
-export default App;
+class ShowList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {this.props.names.map((friend) => {
+            return <li> {friend} </li>
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default class FriendsContainer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: 'Mohammad Ayub',
+      friends: [
+        'Raza',
+        'Zain'
+      ],
+    }
+
+    this.addFriend = this.addFriend.bind(this)
+  }
+  addFriend(friend) {
+    this.setState((state) => ({
+      friends: state.friends.concat([friend])
+    }))
+  }
+  render() {
+    return (
+      <div>
+        <h3> Name: {this.state.name} </h3>
+        <AddFriend addNew={this.addFriend} />
+        <ShowList names={this.state.friends} />
+      </div>
+    )
+  }
+}
